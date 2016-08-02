53a8e03edd9fa1f4d46cce2a7e484b656cda0902
msosa-townsend <miguel.sosa@townsendsecurity.com>
2016-08-01 21:19:55 -0400

    clean up rails 5 deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,6 @@ module Telesocia
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Use pure sql format in schema.rb so it understands the
     # postgresql sepcific code we are adding
     config.active_record.schema_format = :sql
