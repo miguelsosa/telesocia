8c14f73de27ae24530e23f12780209e2029e737b
miguelsosa <miguel.sosa@gmail.com>
2017-02-10 09:45:13 -0500

    change how redirection is done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
 
   # All pages except home pages require authentication
   before_action :authenticate_user!
-end
+
+  before_filter :redirect_if_old
+  protected
+
+  def redirect_if_old
+    if request.host == 'www.telesocia.com'
+      redirect_to "#{request.protocol}telesocia-dev.herokuapp.com#{request.fullpath}", :status => :moved_permanently 
+    end
+  end
+endend
